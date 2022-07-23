8c4df32e2a9e04f27591ec3bc4174a7b5965a886
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-07-23 22:28:26 +0300

    Add basic login
    Signed-off-by: Jenia Sakirko <Jenia.Sakirko@cyberark.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar'; // import navbar
+import Login from './views/auth/Login'; // new
+import Signup from './views/auth/Signup'; // new
+import Logout from './views/auth/Logout'; // new
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+        <div className='App'>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path='/login' element={<Login/>} exact/>
+                    <Route path='/signup' element={<Signup/>} exact/>
+                    <Route path='/logout' element={<Logout/>} exact/>
+                </Routes>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
